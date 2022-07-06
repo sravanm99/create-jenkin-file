e1b4ffc2796fb68e9460b12b295f5da9bdd280bb
shravan <shravanthanay@gmail.com>
2022-07-06 07:55:57 +0530

    changes added

---
+++ src/main/java/com/SJcreatives/sample.java
@@ -0,0 +1,13 @@
+package com.SJcreatives;
+
+/**
+ * Hello sravan!
+ *
+ */
+public class sample 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello sravan!" );
+    }
+}
---
+++ src/test/java/com/SJcreatives/sampleTest.java
@@ -0,0 +1,20 @@
+package com.SJcreatives;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class sampleTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
